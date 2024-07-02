00c7c97832fda289efe5e3a333270c3ccaca3e79
skarppakavalli@gmail.com <skarppakavalli@gmail.com>
2024-07-02 15:14:07 +0530

    feat: added all Logic-2 code files

---
+++ Logic-2/LuckySum.java
@@ -0,0 +1,16 @@
+public class LuckySum {
+    public int luckySum(int a, int b, int c) {
+        int x=a+b+c;
+        if(a==13){
+            x=0;
+        }
+        else if(b==13){
+            x-=b;
+            x-=c;
+        }
+        else if(c==13){
+            x-=c;
+        }
+        return x;
+    }
+}
